6443013b864b88e6405a6c317139505634c8ec1e
rafaelrg0055 <rafaelrg_5@hotmail.com>
2024-06-08 17:52:32 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/rggames/workshopmongo/resources/UserResource.java
@@ -0,0 +1,30 @@
+package com.rggames.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rggames.workshopmongo.domain.User;
+
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){   //Usar RespondeEntity para respostas HTTP com cabeçalho, erros e etc...
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		
+		List<User> list = new ArrayList<>();   // para instanciar interface no Java precisa usar ArrayList
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+}
