77f8b3fb70679ef34929f4776a29340cdccf8711
rafaelrg0055 <rafaelrg_5@hotmail.com>
2024-06-08 14:18:12 -0300

    Iniciando o projeto com MongoDB

---
+++ src/test/java/com/rggames/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rggames.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
