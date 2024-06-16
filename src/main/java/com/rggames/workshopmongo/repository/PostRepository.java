5d8219fe0aad5438c273438c97633f35a18b8d8d
rafaelrg0055 <rafaelrg_5@hotmail.com>
2024-06-16 14:22:20 -0300

    Consulta com varios criterios

--- src/main/java/com/rggames/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rggames/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.rggames.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,4 +16,7 @@ public interface PostRepository extends MongoRepository<Post, String>{   //só c
 	
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")  //Com @Query
 	List<Post> searchTitle(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
